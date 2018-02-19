9216dd70086c4348627e13e9544ee26f5ced5c0a
Stan <rarunok@yahoo.com>
2018-02-18 22:55:54 -0500

    added M_assign function

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -36,8 +36,8 @@
       ;check while statement
       ((eq? (car statement) 'while) (M_while (cadr statement) (car (cddr statement)) state))
       ; check declare statement
-      ((eq? (car statement) 'var) M_declare (cdr statement) state)
-      ((eq? (car statement) '=) (M_assign (cadr statement) (car (cddr (statement))) (car state) (cadr state))) )))
+      ((eq? (car statement) 'var) (M_declare (cdr statement) state))
+      ((eq? (car statement) '=) (M_assign (cadr statement) (car (cddr statement)) (car state) (cadr state))) )))
       
 ; evaluate the while statement returns state
 (define M_while
@@ -52,7 +52,7 @@
     (cond
       ((eq? condition 'true) #t)
       ((eq? condition 'false) #f)
-      ((atom? condition) (find condition (car state) (cadr state)))
+      ((isAtom condition) (find_val condition (car state) (cadr state)))
       ((eq? (car condition) '==) (eq? (M_value (cadr condition) state) (M_value (car (cddr condition)) state)))
       ((eq? (car condition) '>) (> (M_value (cadr condition) state) (M_value (car (cddr condition)) state)))
       ((eq? (car condition) '<) (< (M_value (cadr condition) state) (M_value (car (cddr condition)) state)))
@@ -69,7 +69,7 @@
     (cond
       ((null? expression) ())
       ((number? expression) expression)
-      ((atom? expression) (find expression (car state) (cadr state)))
+      ((isAtom expression) (find_val expression (car state) (cadr state)))
       ((eq? (car expression) '+) (+ (M_value (cadr expression) state) (M_value (car (cddr expression)) state)))
       ((eq? (car expression) '-) (cond
                                    ((eq? (cddr expression) '()) (- 0 (M_value (cadr expression) state)))
@@ -78,28 +78,57 @@
       ((eq? (car expression) '/) (quotient (M_value (cadr expression) state) (M_value (car (cddr expression)) state)))
       ((eq? (car expression) '%) (remainder (M_value (cadr expression) state) (M_value (car (cddr expression)) state)))
       ((number? (car expression)) (car expression))
-      ((atom? (car expression)) (find (car expression) (car state) (cadr state)))
+      ((isAtom (car expression)) (find_val (car expression) (car state) (cadr state)))
       ((list? (car expression)) (M_value (car expression) state)) )))
 
 (define M_declare
   (lambda (statement state)
     (cond
       ; edge case: var already declared
-      ((declared? (car statement) (car state)) (error "This variable has already been declared."))
+      ((isDeclared (car statement) (car state)) (error "This variable has already been declared."))
       ; case 1: declare a var but not assign any value
       ((null? (cdr statement)) (add (car statement) '() (car state) (cadr state)))
       ; case 2: declare a var and assign a boolean to it
-      ((isBooleanOperation? (cadr statement) (car state) (cadr state))
+      ((isBooleanOperation (cadr statement) (car state) (cadr state))
         (add (car statement) (M_boolean (cadr statement) state) (car state) (cadr state)))
       ; case 3: declare a var and assign the val of another var to it
       ((and (list? (cadr statement)) (eq? (car (cadr statement)) '=))
-        (add (car statement) (find (cadr (cadr statement))
+        (add (car statement) (find_val (cadr (cadr statement))
                                    (car (M_state (cadr statement) state))
                                    (cadr (M_state (cadr statement) state))) ; find the val associated with the var
                              (car (M_state (cadr statement) state))
                              (cadr (M_state (cadr statement) state)) )) ; add val & name to state lists
       ; case 4: declare a var and assign num or an expression to it
       (else (add (car statement) (M_value (cadr statement) state) (car state) (cadr state))) )))
+
+; check if the variable is already assigned or not
+(define M_assign
+  (lambda (name expression namelist valuelist)
+    (cond
+      ; edge case: var not declared
+      ((not (isDeclared name namelist))
+        (error "error: using before declaring"))
+      ; edge case: expression is null
+      ((null? expression) 
+        (error "error: null assignment is currently not supported"))
+      ((and (list? expression) (eq? (car expression) '=)) 
+        (M_assign name 
+                  (find_val (cadr expression)
+                            (car (M_state expression (cons namelist (cons valuelist '()))))
+                            (cadr (M_state expression (cons namelist (cons valuelist '())))))
+                  (car (M_state expression (cons namelist (cons valuelist '()))))
+                  (cadr (M_state expression (cons namelist (cons valuelist '()))))))
+      ; case 1: expression is boolean
+      ((isBooleanOperation expression namelist valuelist) 
+        (add name 
+             (M_boolean expression (cons namelist (cons valuelist '())))
+             (car (clear name namelist valuelist)) 
+             (cadr (clear name namelist valuelist)) ))
+      ; case 2: expression is a numerical expression/number
+      (else (add name 
+                 (M_value expression (cons namelist (cons valuelist '())))
+                 (car (clear name namelist valuelist)) 
+                 (cadr (clear name namelist valuelist)))) )))
 
 ; check the expression is boolean or numeric, #t if is boolean
 (define isBooleanOperation
@@ -108,7 +137,7 @@
       ((null? expression) #f)
       ((number? expression) #f)
       ((boolean? expression) #t)
-      ((atom? expression) (isBooleanOperation (find expression namelist valuelist) namelist valuelist))
+      ((isAtom expression) (isBooleanOperation (find_val expression namelist valuelist) namelist valuelist))
       ((eq? (car expression) '&&) #t)
       ((eq? (car expression) '||) #t)
       ((eq? (car expression) '!) #t)
@@ -124,22 +153,22 @@
       (else #f) )))
 
 ; find the value by given name, return error when not declared or not initialized 
-(define find
+(define find_val
   (lambda (name namelist valuelist)
-    (cond
-      ((declared? name namelist) (initialized? name namelist valuelist))
-      (else (error "error: you need to declare a variable first")) )))
+    (if (not (isDeclared name namelist))
+      (error "error: you need to declare a variable first")
+      (isInitialized name namelist valuelist) )))
 
 ; check if the name is declared
-(define declared?
+(define isDeclared
   (lambda (name namelist)
     (cond
       ((null? namelist) #f)
       ((eq? name (car namelist)) #t)
-      (else (declared? name (cdr namelist))) )))
+      (else (isDeclared name (cdr namelist))) )))
 
 ; check if the variable is initialized or not, if it is initialized return the value, else return error
-(define initialized?
+(define isInitialized
   (lambda (name namelist valuelist)
     (cond
       ((null? namelist) (error "variable is not declared"))
@@ -147,7 +176,7 @@
             (cond
               ((not (null? (car valuelist))) (car valuelist))
               (else (error "error: variable has not been assigned a value" ))))
-      (else (initialized? name (cdr namelist) (cdr valuelist))) )))
+      (else (isInitialized name (cdr namelist) (cdr valuelist))) )))
 
 ; clear the previously assigned name and value of the variable, add the new ones later
 (define clear
@@ -164,7 +193,7 @@
   (lambda (name value namelist valuelist)
     (cons (cons name namelist) (cons (cons value valuelist) '())) ))
 
-; atom?
-(define atom?
+; isAtom
+(define isAtom
   (lambda (x)
     (and (not (pair? x)) (not (null? x))) ))
