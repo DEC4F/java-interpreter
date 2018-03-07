ed2b15a7bf4af1d2abc7adca5d6adf0d06addbfb
Stan <rarunok@yahoo.com>
2018-03-06 20:05:14 -0500

    reformatted files

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -5,227 +5,211 @@
 
 ; include parse tree
 (load "simpleParser.scm")
+(load "State.rkt")
 
 ; interpret the grammar and calculate output
 (define interpret
   (lambda (file)
-    (evaluate (parser file) '(() ())) ))
+    (evaluate
+     (call/cc
+      (lambda (return)
+        (M_state (parser file) (base) return (lambda (v) (error "ERROR: Invalid Continue")) (lambda (v) (error "ERROR: Invalid Break")) (lambda (v) (error: "ERROR: Unexpected Error" v)))
+        )))))
 
 ; evaluate the statemet in the parse tree if every statement is evaluated, return the state which will store the output
 (define evaluate
-  (lambda (parse_tree state)
+  (lambda (x)
     (cond
-      ((null? parse_tree) state)
-      (else (evaluate (cdr parse_tree) (M_state (car parse_tree) state))) )))
+      ((eq? x #t) 'true)
+      ((eq? x #f) 'false)
+      ((eq? x 'undeclared) (error "ERROR: Undeclared variable!"))
+      ((eq? x 'uninitialized) (error "ERROR: Uninitialized variable!")) 
+      (else x))))
 
 ; check each type of statement and update the state
 (define M_state
-  (lambda (statement state)
+  (lambda (statement state return cont break throw)
     (cond
-      ;Edge case: no statement, return error
-      ((null? statement) (error "Error Code: EMPTY_STATEMENT"))
-      ;Case 1: check if statement
-      ((eq? (car statement) 'if)
-       (cond
-         ((M_boolean (cadr statement) state) (M_state (caddr statement) state))
-         ((null? (cdddr statement)) state)
-         (else (M_state (cadddr statement) state))))
-      ;Case 2: check return statement
-      ((eq? (car statement) 'return)
-       (cond
-         ((not (isBooleanOperation (cadr statement) (car state) (cadr state))) (M_value (cadr statement) state))
-         ((M_boolean (cadr statement) state) 'true)
-         (else 'false) ))
-      ;Case 3: check while statement
-      ((eq? (car statement) 'while) (M_while (cadr statement) (caddr statement) state))
-      ;Case 4: check declare statement
-      ((eq? (car statement) 'var) (M_declare (cdr statement) state))
-      ;Case 5: check assign statement
-      ((eq? (car statement) '=) (M_assign (cadr statement) (caddr statement) (car state) (cadr state))) )))
+      ;Edge case 1: empty statement
+      ((null? statement) state)
       
-; evaluate the while statement returns state
-(define M_while
-  (lambda (condition expression state)
-    (cond
-      ((M_boolean condition state) (M_while condition expression (M_state expression state)))
-      (else state) )))
+      ;Edge case 2: statement is an atom
+      ((isAtom statement) state)
+      
+      ;Case 0; check list of statements
+      ((list? (first statement)) (M_statement_list statement state return cont break throw))
+      
+      ;Case 1: check if statement
+      ((eq? (first statement) 'if) (M_if statement state return cont break throw))
 
-; evaluates the condition
-(define M_boolean
-  (lambda (condition state)
+      ;Case 2: check return statement
+      ((eq? (first statement) 'return) (M_return statement state return cont break throw))
+      
+      ;Case 3: check while statement
+      ;((eq? (car statement) 'while) (M_while statement state return cont break throw))
+      
+      ;Case 4: check declare statement
+      ((eq? (first statement) 'var) (M_declare statement state return cont break throw))
+      
+      ;Case 5: check assign statement
+      ((eq? (first statement) '=) (M_assign statement state return cont break throw))
+
+      ;Case 6: check throw
+      ((eq? 'throw (first statement)) (throw (second statement) state))
+
+      ;Case 7: check break
+      ((eq? 'break (first statement)) (break state))
+
+      ;Case 8: check continue
+      ((eq? 'continue (first statement)) (cont state))
+
+      ;Case 9: check begin
+      ((eq? 'begin (first statement)) (M_block statement state return cont break throw))
+
+      (else state)
+      )))
+      
+;interprets list of statement
+(define M_statement_list
+  (lambda (statements state return cont break throw)
+    (if (null? statements) state
+        (M_statement_list (rest statements) (M_state (first statements) state return cont break throw) return cont break throw))))
+
+;interprets if statement
+(define M_if
+  (lambda (statement state return cont break throw)
     (cond
-      ;Case 1: condition is true
-      ((eq? condition 'true) #t)
-      ;Case 2: condition is false
-      ((eq? condition 'false) #f)
-      ;Case 3: condition is a variable
-      ((isAtom condition) (find_val condition (car state) (cadr state)))
-      ;Case 4: condition is the result of ==
-      ((eq? (car condition) '==) (eq? (M_value (cadr condition) state) (M_value (caddr condition) state)))
-      ;Case 5: condition is the result of >
-      ((eq? (car condition) '>) (> (M_value (cadr condition) state) (M_value (caddr condition) state)))
-      ;Case 6: condition is the result of <
-      ((eq? (car condition) '<) (< (M_value (cadr condition) state) (M_value (caddr condition) state)))
-      ;Case 7: condition is the result of >=
-      ((eq? (car condition) '>=) (>= (M_value (cadr condition) state) (M_value (caddr condition) state)))
-      ;Case 8: condition is the reult of <=
-      ((eq? (car condition) '<=) (<= (M_value (cadr condition) state) (M_value (caddr condition) state)))
-      ;Case 9: condition is the result of !=
-      ((eq? (car condition) '!=) (not (= (M_value (cadr condition) state) (M_value (caddr condition) state))))
-      ;Case 10: condition is the result of ||
-      ((eq? (car condition) '||) (or (M_boolean (cadr condition) state) (M_boolean (caddr condition) state)))
-      ;Case 11: condition is the result of &&
-      ((eq? (car condition) '&&) (and (M_boolean (cadr condition) state) (M_boolean (caddr condition) state)))
-      ;Case 12: condition is the result of !
-      ((eq? (car condition) '!) (not (M_boolean (cdr condition) state))) )))
+      ((isDeclared statement state) (error 'variableUndeclared "ERROR: Undeclared variable"))
+      ((isInit statement state) (error 'variableUninitialized "ERROR: Uninitialized variable"))
+      ((M_value (second statement) state) (M_state (third statement) state return cont break throw))
+      ((hasElse? statement) (M_state (else_branch statement) state return cont break throw))
+      (else state))))
+
+;interprets return statement
+(define M_return
+  (lambda (statement state return cont break throw)
+    (return (M_value (second statement) state))))
+
+; need to fix
+; evaluate the while statement returns state
+;(define M_while
+  ;(lambda (condition expression state)
+    ;(cond
+      ;((M_value condition state) (M_while condition expression (M_state expression state)))
+      ;(else state) )))
+
+
 
 ; calculate the arithmetic expressions and return the number after calculation
 (define M_value
   (lambda (expression state)
     (cond
       ;Base Case: no expression, return empty
-      ((null? expression) ())
+      ((null? expression) '())
       ;Case 1: the value is the number
       ((number? expression) expression)
       ;Case 2: the value is the value of the variable
-      ((isAtom expression) (find_val expression (car state) (cadr state)))
-      ;Case 3: the value is the result of +
-      ((eq? (car expression) '+) (+ (M_value (cadr expression) state) (M_value (caddr expression) state)))
-      ;Case 4: the value is the result of -
-      ((eq? (car expression) '-) (cond
-                                   ((eq? (cddr expression) '()) (- 0 (M_value (cadr expression) state)))
-                                   (else (- (M_value (cadr expression) state) (M_value (caddr expression) state))) ))
-      ;Case 5: the value is the result of *
-      ((eq? (car expression) '*) (* (M_value (cadr expression) state) (M_value (caddr expression) state)))
-      ;Case 6: the value is the result of quotient
-      ((eq? (car expression) '/) (quotient (M_value (cadr expression) state) (M_value (caddr expression) state)))
-      ;Case 7: the value is the result of remainder
-      ((eq? (car expression) '%) (remainder (M_value (cadr expression) state) (M_value (caddr expression) state)))
-      ;Case 8: this is for when we have "=" in a expression
-      ((eq? (car expression) '=) (M_value (cadr expression) (M_state expression state)) )
-      ;Case 9: the value is the number wrapped in ()
-      ((number? (car expression)) (car expression))
-      ;Case 10: the value is the value of variable wrapped in ()
-      ((isAtom (car expression)) (find_val (car expression) (car state) (cadr state)))
-      ;Case 11: the value is the value of the element inside list
-      ((list? (car expression)) (M_value (car expression) state)) )))
+      ((isAtom expression) (lookup_list expression state))
+      ;Case 3:
+      ((null? (second expression)) (M_value (first statement) state))
 
+      ;not sure
+      ((or (eq? 'uninitialized (M_value (second expression) state)) (eq? 'undeclared (M_value (second expression) state))) (error "ERROR: Variable not declared or initialized"))
+      ((and (not (null? (cddr expression))) (or (eq? 'uninitialized (M_value (third expression) state)) (eq? 'undeclared (M_value (third expression) state)))) (error "ERROR: Variable not declared or initialized!"))
+      
+      ;Case 4: the value is the result of +
+      ((eq? (operator expression) '+) (+ (M_value (cadr expression) state) (M_value (caddr expression) state)))
+      ;Case 5: the value is the result of -
+      ((eq? (operator expression) '-) (cond
+                                   ((eq? (cddr expression) '()) (- 0 (M_value (second expression) state)))
+                                   (else (- (M_value (second expression) state) (M_value (third expression) state))) ))
+      ;Case 6: the value is the result of *
+      ((eq? (operator expression) '*) (* (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 7: the value is the result of quotient
+      ((eq? (operator expression) '/) (quotient (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 8: the value is the result of remainder
+      ((eq? (operator expression) '%) (remainder (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 9: this is for when we have "=" in a expression
+      ;((eq? (operator expression) '=) (M_value (second expression) (M_state expression state)) )    
+      ;Case 10: expression is the result of ==
+      ((eq? (operator expression) '==) (eq? (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 11: expression is the result of >
+      ((eq? (operator expression) '>) (> (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 12: expression is the result of <
+      ((eq? (operator expression) '<) (< (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 13: expression is the result of >=
+      ((eq? (operator expression) '>=) (>= (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 14: expression is the reult of <=
+      ((eq? (operator expression) '<=) (<= (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 15: expression is the result of !=
+      ((eq? (operator expression) '!=) (not (= (M_value (second expression) state) (M_value (third expression) state))))
+      ;Case 16: expression is the result of ||
+      ((eq? (operator expression) '||) (or (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 17: expression is the result of &&
+      ((eq? (operator expression) '&&) (and (M_value (second expression) state) (M_value (third expression) state)))
+      ;Case 28: expression is the result of !
+      ((eq? (operator expression) '!) (not (M_value (second expression) state)))
+      )))
+
+; Declare variable
 (define M_declare
-  (lambda (statement state)
+  (lambda (statement state return cont break throw)
     (cond
       ; edge case: var already declared
-      ((isDeclared (car statement) (car state)) (error "Error Code: REDEFINING_VARIABLE"))
+      ((inState? (second statement) state) (error "Error Code: REDEFINING_VARIABLE"))
       ; case 1: declare a var but not assign any value
-      ((null? (cdr statement)) (add (car statement) '() (car state) (cadr state)))
-      ; case 2: declare a var and assign a boolean to it
-      ((isBooleanOperation (cadr statement) (car state) (cadr state))
-        (add (car statement) (M_boolean (cadr statement) state) (car state) (cadr state)))
-      ; case 3: declare a var and assign the val of another var to it
-      ((and (list? (cadr statement)) (eq? (caadr statement) '=))
-        (add (car statement) (find_val (cadr (cadr statement))
-                                       (car (M_state (cadr statement) state))
-                                       (cadr (M_state (cadr statement) state))) ; find the val associated with the var
-                             (car (M_state (cadr statement) state))
-                             (cadr (M_state (cadr statement) state)) )) ; add val & name to state lists
-      ; case 4: declare a var and assign num or an expression to it
-      (else (add (car statement) (M_value (cadr statement) state) (car state) (cadr state))) )))
+      ((null? (rest_after_two statement)) (add_to_state (second statement) 'uninitialized state))
+      ; case 2: declare a var and assign a value to it
+      (else (add_to_state (second statement) (M_value (third statement) state) (M_state (third statement) state return cont break throw))) 
+       )))
 
-; check if the variable is already assigned or not
+; Assign value
 (define M_assign
-  (lambda (name expression namelist valuelist)
-    (cond
-      ; edge case: var not declared
-      ((not (isDeclared name namelist))
-        (error "Error Code: USING_BEFORE_DECLARING"))
-      ; edge case: expression is null
-      ((null? expression) 
-        (error "Error Code: NULL_ASSIGNMENT"))
-      ; case 1: expression contains nested expression, including use of var
-      ((and (list? expression) (eq? (car expression) '=)) 
-        (M_assign name 
-                  (find_val (cadr expression)
-                            (car (M_state expression (cons namelist (cons valuelist '()))))
-                            (cadr (M_state expression (cons namelist (cons valuelist '())))))
-                  (car (M_state expression (cons namelist (cons valuelist '()))))
-                  (cadr (M_state expression (cons namelist (cons valuelist '()))))))
-      ; case 2: expression is boolean
-      ((isBooleanOperation expression namelist valuelist) 
-        (add name 
-             (M_boolean expression (cons namelist (cons valuelist '())))
-             (car (clear name namelist valuelist)) 
-             (cadr (clear name namelist valuelist)) ))
-      ; case 3: expression is a numerical expression/number
-      (else (add name 
-                 (M_value expression (cons namelist (cons valuelist '())))
-                 (car (clear name namelist valuelist)) 
-                 (cadr (clear name namelist valuelist)))) )))
+  (lambda (statement state return cont break throw)
+    (if (inState? (second statement) state)
+        (add_to_state (second statement) (M_value (third statement) state) state)
+        (error "Error Code: USING_BEFORE_DECLARING")
+        )))
+
+(define M_block
+  (lambda (statement state return cont break throw)
+    (pop_layer (M_statement_list (rest statement) (push_layer state) return (lambda (w) (cont (pop_layer w))) (lambda (w) (break (pop_layer w))) (lambda (w s) (throw w (pop_layer s)))))))
 
 
-; following functions are the helper methods
-; check the expression is boolean or numeric, #t if is boolean
-(define isBooleanOperation
-  (lambda (expression namelist valuelist)
-    (cond
-      ((null? expression) #f)
-      ((number? expression) #f)
-      ((boolean? expression) #t)
-      ((isAtom expression) (isBooleanOperation (find_val expression namelist valuelist) namelist valuelist))
-      ((eq? (car expression) '&&) #t)
-      ((eq? (car expression) '||) #t)
-      ((eq? (car expression) '!) #t)
-      ((eq? (car expression) '>=) #t)
-      ((eq? (car expression) '==) #t)
-      ((eq? (car expression) '<=) #t)
-      ((eq? (car expression) '>) #t)
-      ((eq? (car expression) '<) #t)
-      ((eq? (car expression) '!=) #t)
-      ((eq? (car expression) 'true) #t)
-      ((eq? (car expression) 'false) #t)
-      ((list? (car expression)) (isBooleanOperation (car expression) namelist valuelist))
-      (else #f) )))
+; -----------------------------------------Abstraction-------------------------------------------------------
+;(define first car)
+(define condition car)
+;(define second cadr)
+;(define third caddr)
+(define else_branch cadddr)
+(define operator car)
+;(define rest cdr)
+(define rest_after_two cddr)
 
-; find the value by given name, return error when not declared or not initialized 
-(define find_val
-  (lambda (name namelist valuelist)
-    (if (not (isDeclared name namelist))
-      (error "Error Code: USING_BEFORE_DECLARING")
-      (isInitialized name namelist valuelist) )))
+; ---------------------------------------The followings are the helper methods--------------------------------
 
-; check if the name is declared
+; Check if the name is declared
 (define isDeclared
-  (lambda (name namelist)
-    (cond
-      ((null? namelist) #f)
-      ((eq? name (car namelist)) #t)
-      (else (isDeclared name (cdr namelist))) )))
-
-; check if the variable is initialized or not, if it is initialized return the value, else return error
-(define isInitialized
-  (lambda (name namelist valuelist)
-    (cond
-      ((null? namelist) (error "Error Code: USING_BEFORE_DECLARING"))
-      ((eq? name (car namelist))
-            (cond
-              ((not (null? (car valuelist))) (car valuelist))
-              (else (error "Error Code: USING_BEFORE_ASSIGNING" ))))
-      (else (isInitialized name (cdr namelist) (cdr valuelist))) )))
-
-; clear the previously assigned name and value of the variable, add the new ones later
-(define clear
-  (lambda (name namelist valuelist)
-    (cond
-      ((null? namelist) '(()()))
-      ((eq? name (car namelist)) (list (car (clear name (cdr namelist) (cdr valuelist)))
-                                       (cadr (clear name (cdr namelist) (cdr valuelist))) ))
-      (else (list (cons (car namelist) (car (clear name (cdr namelist) (cdr valuelist))))
-                  (cons (car valuelist) (cadr (clear name (cdr namelist) (cdr valuelist)))))) )))
-
-; add the name and value to state, all conditions are checked before adding
-(define add
-  (lambda (name value namelist valuelist)
-    (cons (cons name namelist) (cons (cons value valuelist) '())) ))
+  (lambda (statement state)
+    (eq? 'undeclared (M_value (second statement) state))
+     ))
 
 ; isAtom, check whether the element is an atom
 (define isAtom
   (lambda (x)
     (and (not (pair? x)) (not (null? x))) ))
+
+; Checks to see if a variable is already in the state
+(define inState?
+  (lambda (name state)
+    (not (eq? 'undeclared (lookup_list name state))) ))
+
+; Checks to see if the statement is in the environment 
+(define isInit
+  (lambda (statement state)
+    (eq? 'uninitialized (M_value (second statement) state)) ))
+
+; Check else part in if statement
+(define hasElse?
+  (lambda (statement)
+    (not (null? (cdddr statement))) ))
