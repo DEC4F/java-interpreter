30ddc43dfb47092890782439bec2d1040c0144f0
DEC4F <rarunok@yahoo.com>
2018-05-18 09:04:12 -0400

    deleted old versions, changed README.md

--- Part_2/Test 3.txt
+++
@@ -1,7 +0,0 @@
-var x = 0;
-var y = 10;
-while (!(x >= y) || !(y > 25)) {
-  x = x + 2;
-  y = y + 1;
-}
-return x;
---
+++ Test.scm
@@ -0,0 +1,32 @@
+(load "Interpreter.scm")
+
+(define test
+  (lambda ()
+    (display
+     (if (string=? (string-append (test-4)) "")
+         "\n All Tests Have Passed! \n\n"
+         (string-append (test-4))))))
+
+(define test-4
+  (lambda ()
+    (string-append
+     (assert-equal "Test/Test_1.txt" 'A 15)
+     (assert-equal "Test/Test_2.txt" 'A 12)
+     (assert-equal "Test/Test_3.txt" 'A 125)
+     (assert-equal "Test/Test_4.txt" 'A 36)
+     (assert-equal "Test/Test_5.txt" 'A 54)
+     (assert-equal "Test/Test_6.txt" 'A 110)
+     (assert-equal "Test/Test_7.txt" 'C 26)
+     (assert-equal "Test/Test_8.txt" 'Square 117)
+     (assert-equal "Test/Test_9.txt" 'Square 32)
+     (assert-equal "Test/Test_10.txt" 'List 15)
+     (assert-equal "Test/Test_11.txt" 'List 123456)
+     (assert-equal "Test/Test_12.txt" 'List 5285)
+     ;(assert-equal "Test/Test_13.txt" 'C -716)
+     )))
+
+(define assert-equal
+  (lambda (file class expected-value)
+    (if (eq? (interpret file class) expected-value)
+        ""
+(string-append file " FAILED\n"))))
